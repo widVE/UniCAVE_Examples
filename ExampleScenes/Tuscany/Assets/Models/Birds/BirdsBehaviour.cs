cf54a092e058a5b58e29ad758ad23cfa322c37b9
Simon <spsmith5@wisc.edu>
2020-06-18 11:24:21 -0500

    Keeping only working 2019 examples
    Keeping only the OR and Tuscany examples for now. Other examples will be added back when they are ready.

--- ExampleScenes/Tuscany/Assets/Models/Birds/BirdsBehaviour.cs
+++ ExampleScenes/Tuscany/Assets/Models/Birds/BirdsBehaviour.cs
@@ -5,8 +5,8 @@ public class BirdsBehaviour : MonoBehaviour
 {
 	public Transform birdsPrefab;
 	float birdTimer;
-	ParticleAnimator animator;
-	ParticleEmitter emitter;
+	//ParticleAnimator animator;
+	//ParticleEmitter emitter;
 	
 	void Start()
 	{
@@ -31,12 +31,12 @@ public class BirdsBehaviour : MonoBehaviour
 //		transform.position = new Vector3( ((Random.Range(0,2)*2)-1) * Random.Range(65f, 80f), transform.position.y, Random.Range(-30f, -20f));
 //		transform.LookAt(new Vector3(0, transform.position.y, Random.Range(-80f, -50f)) );
 		
-		Transform birds = (Transform)Instantiate(birdsPrefab, transform.position, transform.rotation);
+		/*Transform birds = (Transform)Instantiate(birdsPrefab, transform.position, transform.rotation);
 		animator = birds.GetComponentInChildren(typeof(ParticleAnimator)) as ParticleAnimator;
 		animator.force = new Vector3(0, Random.Range(-0.3f, 0.3f), 0);
 		emitter = birds.GetComponentInChildren(typeof(ParticleEmitter)) as ParticleEmitter;
 		emitter.emit = true;
 		
-		birdTimer = Time.time + Random.Range(5,20);
+		birdTimer = Time.time + Random.Range(5,20);*/
 	}
 }
